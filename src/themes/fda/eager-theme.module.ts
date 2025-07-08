f9bd92cb569675f70a6813fa76ced9c00813b65e
Laura Henze <lhenze@users.noreply.github.com>
2025-07-08 14:21:14 -0400

    TopLevelCommunityListComponent: add
    Currently, only using this to hide the pagination info

--- src/themes/fda/eager-theme.module.ts
+++ src/themes/fda/eager-theme.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './app/header/header.component';
 import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
 import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
-
+import { TopLevelCommunityListComponent } from './app/home-page/top-level-community-list/top-level-community-list.component';
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
@@ -19,6 +19,7 @@ const DECLARATIONS = [
   HeaderComponent,
   HeaderNavbarWrapperComponent,
   NavbarComponent,
+  TopLevelCommunityListComponent
 ];
 
 @NgModule({
