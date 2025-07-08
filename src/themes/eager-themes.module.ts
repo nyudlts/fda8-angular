96ac61ce4ca630bab5ea3672ffcb77d1af3b5e29
Laura Henze <lhenze@users.noreply.github.com>
2025-07-08 11:20:09 -0400

    New theme: install and apply

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+import { EagerThemeModule as FDAEagerThemeModule } from './fda/eager-theme.module';
 
 /**
  * This module bundles the eager theme modules for all available themes.
@@ -14,7 +15,7 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
   imports: [
     DSpaceEagerThemeModule,
     // CustomEagerThemeModule,
-  ],
+    FDAEagerThemeModule  ],
 })
 export class EagerThemesModule {
 }
