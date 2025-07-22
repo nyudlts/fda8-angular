ac0a6a8eee2c15ddf349bc0e124959c70c163b90
Laura Henze <lhenze@users.noreply.github.com>
2025-07-22 19:14:31 -0400

    Component: add to theme

--- src/themes/fda/eager-theme.module.ts
+++ src/themes/fda/eager-theme.module.ts
@@ -7,6 +7,7 @@ import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-na
 import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
 import { TopLevelCommunityListComponent } from './app/home-page/top-level-community-list/top-level-community-list.component';
+import { CommunityPageSubCollectionListComponent } from './app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component';
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
@@ -19,7 +20,8 @@ const DECLARATIONS = [
   HeaderComponent,
   HeaderNavbarWrapperComponent,
   NavbarComponent,
-  TopLevelCommunityListComponent
+  TopLevelCommunityListComponent,
+ CommunityPageSubCollectionListComponent,
 ];
 
 @NgModule({
