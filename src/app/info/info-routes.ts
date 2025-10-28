034f45150eeb2fe795a683739425bdbf968540fb
Laura Henze <lhenze@users.noreply.github.com>
2025-10-28 12:09:32 -0400

    Fix syntax for PR
    npm run lint -- --fix

--- src/app/info/info-routes.ts
+++ src/app/info/info-routes.ts
@@ -8,20 +8,20 @@ import { i18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.reso
 import { notifyInfoGuard } from '../core/coar-notify/notify-info/notify-info.guard';
 import { feedbackGuard } from '../core/feedback/feedback.guard';
 import { hasValue } from '../shared/empty.util';
+import { AboutComponent } from './about/about.component';
 import { AccessibilitySettingsComponent } from './accessibility-settings/accessibility-settings.component';
 import { ThemedEndUserAgreementComponent } from './end-user-agreement/themed-end-user-agreement.component';
 import { ThemedFeedbackComponent } from './feedback/themed-feedback.component';
 import {
+  ABOUT_PATH,
   ACCESSIBILITY_SETTINGS_PATH,
   COAR_NOTIFY_SUPPORT,
   END_USER_AGREEMENT_PATH,
   FEEDBACK_PATH,
   PRIVACY_PATH,
-  ABOUT_PATH,
 } from './info-routing-paths';
 import { NotifyInfoComponent } from './notify-info/notify-info.component';
 import { ThemedPrivacyComponent } from './privacy/themed-privacy.component';
-import { AboutComponent } from './about/about.component';
 
 export const ROUTES: Routes = [
   {
