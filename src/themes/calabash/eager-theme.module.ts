f61ac42d88bf33486fb300be9ba2f363e8f08839
ekate <ekate@nyu.edu>
2025-11-02 19:45:53 -0500

    Add custom theme for calabash

--- src/themes/calabash/eager-theme.module.ts
+++ src/themes/calabash/eager-theme.module.ts
@@ -19,7 +19,6 @@ const DECLARATIONS = [
 @NgModule({
   imports: [
     CommonModule,
-
     ...DECLARATIONS,
   ],
   providers: [
--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -5,6 +5,7 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 import { EagerThemeModule as FDAEagerThemeModule } from './fda/eager-theme.module';
 import { EagerThemeModule as GallatinSyllabiEagerThemeModule } from './gallatin-syllabi/eager-theme.module';
+import { EagerThemeModule as CalabashEagerThemeModule } from './calabash/eager-theme.module';
 
 /**
  * This module bundles the eager theme modules for all available themes.
