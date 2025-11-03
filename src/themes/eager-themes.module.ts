1f98e0abf220144d7ec76238da00289a7feae5cd
ekate <ekate@nyu.edu>
2025-11-02 19:45:53 -0500

    Add custom theme for calabash

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -6,7 +6,6 @@ import { EagerThemeModule as FDAEagerThemeModule } from './fda/eager-theme.modul
 import { EagerThemeModule as GallatinSyllabiEagerThemeModule } from './gallatin-syllabi/eager-theme.module';
 import { EagerThemeModule as CalabashEagerThemeModule } from './calabash/eager-theme.module';
 
-
 /**
  * This module bundles the eager theme modules for all available themes.
  * Eager modules contain components that are present on every page (to speed up initial loading)
